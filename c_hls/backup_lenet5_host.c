9ef9332697a3212b2397628b4c7473036dc7af30
hautran_itr_intern <hauken146@gmail.com>
2023-04-01 11:01:23 +0700

    update: change dir of w/data

--- c_hls/backup_lenet5_host.c
+++ c_hls/backup_lenet5_host.c
@@ -24,11 +24,11 @@ int main(int argc, char** argv){
    FILE *fp;
 
     /* Load Weights from DDR->LMM */
-   fp = fopen("../data/weights/w_conv1.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/weights/w_conv1.txt", "r");
    for(i=0;i<6;i++)
        fscanf(fp, "%f ",  &(w_conv1[i][0][0]));  fclose(fp);
 
-   fp = fopen("../data/weights/w_conv2.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/weights/w_conv2.txt", "r");
    for(i=0;i<16;i++){
        for(j=0;j<6;j++){
            for(m=0;m<5;m++){
@@ -41,55 +41,55 @@ int main(int argc, char** argv){
    }
    fclose(fp);
 
-   fp = fopen("../data/weights/w_fc1.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/weights/w_fc1.txt", "r");
    for(i=0;i<120;i++){
        for(j=0;j<400;j++)
            fscanf(fp, "%f ",  &(w_fc1[i][j]));
    }
    fclose(fp);
 
-   fp = fopen("../data/weights/w_fc2.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/weights/w_fc2.txt", "r");
    for(i=0;i<84;i++){
        for(j=0;j<120;j++)
            fscanf(fp, "%f ",  &(w_fc2[i][j]));
    }
    fclose(fp);
 
-   fp = fopen("../data/weights/w_fc3.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/weights/w_fc3.txt", "r");
    for(i=0;i<10;i++){
        for(j=0;j<84;j++)
            fscanf(fp, "%f ",  &(w_fc3[i][j]));
    }
    fclose(fp);
 
-   fp = fopen("../data/weights/b_conv1.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/weights/b_conv1.txt", "r");
    for(i=0;i<6;i++)
        fscanf(fp, "%f ",  &(b_conv1[i]));  fclose(fp);
 
-   fp = fopen("../data/weights/b_conv2.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/weights/b_conv2.txt", "r");
    for(i=0;i<16;i++)
        fscanf(fp, "%f ",  &(b_conv2[i]));  fclose(fp);
 
-   fp = fopen("../data/weights/b_fc1.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/weights/b_fc1.txt", "r");
    for(i=0;i<120;i++)
        fscanf(fp, "%f ",  &(b_fc1[i]));  fclose(fp);
 
-   fp = fopen("../data/weights/b_fc2.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/weights/b_fc2.txt", "r");
    for(i=0;i<84;i++)
        fscanf(fp, "%f ",  &(b_fc2[i]));  fclose(fp);
 
-   fp = fopen("../data/weights/b_fc3.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/weights/b_fc3.txt", "r");
    for(i=0;i<10;i++)
        fscanf(fp, "%f ",  &(b_fc3[i]));  fclose(fp);
 
    float *dataset = (float*)malloc(LABEL_LEN*28*28 *sizeof(float));
    int target[LABEL_LEN];
 
-   fp = fopen("../data/MNIST/mnist-test-target.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/MNIST/mnist-test-target.txt", "r");
    for(i=0;i<LABEL_LEN;i++)
        fscanf(fp, "%d ",  &(target[i]));  fclose(fp);
 
-   fp = fopen("../data/MNIST/mnist-test-image.txt", "r");
+   fp = fopen("C:/Users/Admins/Downloads/New folder/handwritten-digits-recognition-hls-main/data/MNIST/mnist-test-image.txt", "r");
    for(i=0;i<LABEL_LEN*28*28;i++)
        fscanf(fp, "%f ",  &(dataset[i]));  fclose(fp);
 
